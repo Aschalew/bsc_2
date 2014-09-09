e8791bb585aee56c52bd15466efc8e787ee0060f
Aschalew <kiba.adote2002@gmail.com>
2014-09-09 18:23:21 +0300

    new repo first commit

---
+++ BSC_2007/BSC_2007/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSC_2007.Global" Language="C#" %>
---
+++ BSC_2007/BSC_2007/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BSC_2007.Startup))]
+namespace BSC_2007
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
